e1b04de81d9993d2b9323fb34196697a3a73c739
Gadiel Us <gadyus18@gmail.com>
2023-05-06 12:51:26 -0600

    Se agrego la parte modificada del usuario la opcion de firmar ya funciona, solo falta la parte de verificar las firmas pero igual esta cerca de ser terminada

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 require('dotenv').config();
+const multer = require('multer');
+const crypto = require('crypto');
+const fs = require('fs');
 
 //para el uso de EJS
 app.set('view engine', 'ejs');
@@ -93,5 +96,118 @@ app.post("/verificar", (req, res) => {
 
 
 app.get("/secionIniciada", (req, res) =>{
-  res.sendFile(path.join(__dirname, "public", "IniciosecionUser.html"));
+  const inicio ={
+    inicioSecion : true,
+    ruta: 0,
+  }
+  res.render("UserInterface", inicio);
 })
+
+//Dentro de la interfaz del usuario
+
+
+//cargar PDF
+const upload = multer({
+  dest: 'uploads/' // La carpeta donde se guardarán los archivos subidos
+});
+
+app.post('/ruta1', (req, res) =>{
+  const rutauser ={
+    inicioSecion : false,
+    ruta: 1,
+    subirArchivo: true,
+    ErrorPDF: false,
+  }
+  res.render("UserInterface", rutauser);
+});
+
+//Creamos la logica para la firma
+app.post('/firmar',upload.single('pdf'), (req, res)=>{
+  
+  //Son los datos para que renderize correctamente
+  const rutasUser =[
+    {
+      inicioSecion : false,
+      ruta: 1,
+      subirArchivo: true,
+      ErrorPDF: true
+    },
+    {
+      inicioSecion : false,
+      ruta: 1,
+      subirArchivo: false,
+    }
+  ]
+const firmar = true;
+  guardarPDF(req, res, rutasUser, firmar);
+  
+})
+
+
+
+//La segunda ruta es para la comparacion de Las dos Rutas
+app.post('/ruta2', (req, res)=>{
+  
+  const rutauser= {
+    inicioSecion: false,
+    ruta: 2,
+  }
+  res.render("UserInterface", rutauser);
+})
+app.post('/verificar', upload.fields([{ name: 'pdf1', maxCount: 1 }, { name: 'pdf2', maxCount: 1 }]), (req, res) => {
+  
+  
+  const firmar = false;
+  guardarPDF(req, res, rutasUser, firmar);
+  
+});
+
+
+//Funciones
+//generamos la funcion que guardara el PDF
+function guardarPDF(req, res, rutauser, firmar) {
+  if(firmar){
+    const file = req.file;
+
+    if (!file) {
+    
+      return res.render("UserInterface", rutauser[0]);
+    }
+    const extension = path.extname(file.originalname);
+  
+    if (extension === '.pdf') {
+      const pdfPath = path.resolve(file.path);
+      const pdf = fs.readFileSync(pdfPath);
+  
+  
+      rutauser[1].NombrePDF = []; // Crear la propiedad subirArchivo como un array vacío
+      rutauser[1].NombrePDF.push(pdf.name); // Agregar pdfinfo al array
+      rutauser[1].HashPDF = []; 
+      rutauser[1].HashPDF.push(crearHash(pdf)); // Agregar pdfinfo al arra
+      console.log(rutauser[1]);
+      res.render("UserInterface", rutauser[1]);
+  
+    } else {
+      
+      res.render("UserInterface", rutauser[0]);
+    }
+  }else{
+    const pdf1 = req.files['pdf1'][0]; // el primer archivo PDF cargado
+    const pdf2 = req.files['pdf2'][0]; // el segundo archivo PDF cargado
+    if(!pdf1 || !pdf2 ){
+      
+    }
+    
+  }
+}
+
+
+//añadimos la funcion que proporcionara el hash
+function crearHash(file){
+  const hash = crypto.createHash('sha256');
+  hash.update(file);
+const hashValue = hash.digest('hex');
+console.log(hashValue); // Imprime el hash en formato hexadecimal
+return hashValue;
+}
+
